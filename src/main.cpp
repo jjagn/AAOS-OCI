7c2966b5375191b9f9c1a8ef4e194ddb7630060f
Jackson Crawford <72364545+jjagn@users.noreply.github.com>
2024-01-10 16:29:54 +1300

    servo movement early version working and general program flow ok. still need to add logic for nose and lever lighting

--- src/main.cpp
+++ src/main.cpp
@@ -2,189 +2,201 @@
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Servo.h>
 
-#define LED_DATA    D0 // LED data out pin
+#define LED_DATA    D10 // LED data out pin
 #define NUMPIXELS   240 // 60 LED/meter 4 metre strip
 #define DELAY       0 // 10ms delay
 
-/*
-    ANIMATION VARIABLES
+#define SERVO_PIN   D7 // servo pin
+#define SERVO_SMOOTHING 0.0001 // smoothing value for servo, smaller = smoother
+Servo servo = Servo();
+float servoTarget = 0;
+float servoPrevious = 0;
+float servoSmoothed = 0;
+
+/* 
+        ANIMATION
 */
 
-// fps of program states
+// animation framerate
 #define FRAMES_PER_SECOND 120
-
-// number of different animation states
+// number of animation stages
 #define NUM_STAGES 10
-
-// time to complete one full animation, e.g. cycle through all states IN SECONDS
-#define CYCLE_TIME 10
-
-// cycle time in milliseconds
-#define CYCLE_TIME_MS (CYCLE_TIME * 1000)
-
-// time per frame IN MILLISECONDS
-#define FRAME_TIME_MS (1000 / FRAMES_PER_SECOND)
-
+// total run time of animation (seconds)
+#define ANIMATION_RUNTIME 10
 // total number of frames in animation
-#define TOTAL_FRAMES (FRAMES_PER_SECOND * CYCLE_TIME)
+#define MAX_FRAMES (FRAMES_PER_SECOND * ANIMATION_RUNTIME)
+// frames in each animation stage
+#define FRAMES_PER_STAGE (MAX_FRAMES / NUM_STAGES)
+// frame time in microseconds
+#define FRAME_TIME_US ((1000 * 1000) / FRAMES_PER_SECOND)
+// pin for hall sensor
+#define HALL 2
+// ADC level for magnet detection
+#define HALL_THRESHOLD 2900
+// maxmimum pacer value for hall sensor analogRead
+#define HALL_PACER_MAX 5000
 
-// frames per stage
-#define FRAMES_PER_STAGE (TOTAL_FRAMES / NUM_STAGES)
+// stage beginning and end frames
+#define STAGE_1_START   (FRAMES_PER_STAGE * 0)
+#define STAGE_1_END     (FRAMES_PER_STAGE * 1)
+#define STAGE_2_START   (FRAMES_PER_STAGE * 1)
+#define STAGE_2_END     (FRAMES_PER_STAGE * 2)
+#define STAGE_3_START   (FRAMES_PER_STAGE * 2)
+#define STAGE_3_END     (FRAMES_PER_STAGE * 3)
+#define STAGE_4_START   (FRAMES_PER_STAGE * 3)
+#define STAGE_4_END     (FRAMES_PER_STAGE * 4)
+#define STAGE_5_START   (FRAMES_PER_STAGE * 4)
+#define STAGE_5_END     (FRAMES_PER_STAGE * 5)
+#define STAGE_6_START   (FRAMES_PER_STAGE * 5)
+#define STAGE_6_END     (FRAMES_PER_STAGE * 6)
+#define STAGE_7_START   (FRAMES_PER_STAGE * 6)
+#define STAGE_7_END     (FRAMES_PER_STAGE * 7)
+#define STAGE_8_START   (FRAMES_PER_STAGE * 7)
+#define STAGE_8_END     (FRAMES_PER_STAGE * 8)
+#define STAGE_9_START   (FRAMES_PER_STAGE * 8)
+#define STAGE_9_END     (FRAMES_PER_STAGE * 9)
+#define STAGE_10_START   (FRAMES_PER_STAGE * 9)
+#define STAGE_10_END    (FRAMES_PER_STAGE * 10)
 
-int state; // controls program state
-long masterFrame; // counts up program frames
-long prevFrame = 0; // previous frame
-long frameOffset = 0;
+long frame = 0;
+long prevFrame = 0;
+long frameStartTime = 0;
 
-// int totalFrames = FRAMES_PER_SECOND * CYCLE_TIME;
-// int framesPerStage = totalFrames / NUM_STAGES;
-
-long tCurrent; // current program time in milliseconds
-long tOffset = 0; // used to offset the program by a certain time and maintain sync with the lower half
-
+// animation functions
+void runLever(long frame);
+void runNoseLighting(long frame);
+void runLeverLighting(long frame);
+float servoLinear(long frame, long startFrame, long endFrame, long startPos, long endPos);
 
 /*
-    PIN DEFINITIONS
+        FILTERING
 */
 
-#define HALL 2
+#define WINDOW_SIZE 10
 
+int idx = 0;
+long sum = 0;
+long readings[WINDOW_SIZE];
+long averaged = 0;
 
-// R 125, G 0, B 118
-
-// Adafruit_NeoPixel pixels(NUMPIXELS, LED_DATA, NEO_GRB + NEO_KHZ800);
-
-// void colorWipe(uint32_t color, int wait);
-// void theaterChase(uint32_t color, int wait);
-// void rainbow(int wait);
-// void theaterChaseRainbow(int wait);
 
 void setup() {
-//   pixels.begin(); // INITIALIZE NeoPixel strip object
-//   pixels.clear();
-//   pixels.show();
-//   pixels.setBrightness(255);
-    Serial.begin(460800); 
-    pinMode(HALL, INPUT_PULLDOWN);      // hall sensor output active high
+    // put your setup code here, to run once:
+    pinMode(HALL, INPUT_PULLDOWN);
+    Serial.begin(460800);
+    pinMode(SERVO_PIN, OUTPUT);
+    servo.write(SERVO_PIN, 0);
 }
 
 void loop() {
-    // get time
-    tCurrent = millis();
-    masterFrame = tCurrent / (FRAME_TIME_MS);
-    int frame = (masterFrame - frameOffset) % TOTAL_FRAMES;
-
-    // decide on state from frame number    
-    if (frame < FRAMES_PER_STAGE) state = 1;
-    else if (frame >= FRAMES_PER_STAGE && frame < 2*FRAMES_PER_STAGE) state = 2;
-    else if (frame >= 2*FRAMES_PER_STAGE && frame < 3*FRAMES_PER_STAGE) state = 3;
-    else if (frame >= 3*FRAMES_PER_STAGE && frame < 4*FRAMES_PER_STAGE) state = 4;
-    else if (frame >= 4*FRAMES_PER_STAGE && frame < 5*FRAMES_PER_STAGE) state = 5;
-    else if (frame >= 5*FRAMES_PER_STAGE && frame < 6*FRAMES_PER_STAGE) state = 6;
-    else if (frame >= 6*FRAMES_PER_STAGE && frame < 7*FRAMES_PER_STAGE) state = 7;
-    else if (frame >= 7*FRAMES_PER_STAGE && frame < 8*FRAMES_PER_STAGE) state = 8;
-    else if (frame >= 8*FRAMES_PER_STAGE && frame < 9*FRAMES_PER_STAGE) state = 9;
-    else if (frame >= 9*FRAMES_PER_STAGE) state = 10;
-
-    // Serial.println("state:");
-    // Serial.println(state);
-
-
-    switch (state)
-    {
-    // state 1
-    case 1:
-        // servo open slowly
-        /* code */
-        break;
-
-    // state 2
-    case 2:
-        // STEPPER:
-        // set target to open
-
-        break;
-
-    // state 3
-    case 3:
-
-        break;
-
-    case 4:
-    
-        break;
-
-    case 5:
-        // STEPPER:
-        // set target to closed
-
-        break;
-
-    case 6:
-
-        break;
-
-    case 7:
-
-        break;
-
-    case 8:
-
-        break;
-
-    case 9:
-
-        break;
-
-    case 10:
-        Serial.print("monitoring hall: ");
-        Serial.println(analogRead(HALL));
-        if (analogRead(HALL) >= 3500) { // 4095 max
-            state = 1; // reset animation to beginning
-            frameOffset = millis() / FRAME_TIME_MS; // take final tOffset reading
+    // animation timing and repeat management
+    if (frame <= MAX_FRAMES) {
+        if ((micros() - frameStartTime) >= FRAME_TIME_US) {
+            frame++;
+            frameStartTime = micros();
         }
-        break;
+        if (frame > prevFrame) {
+            Serial.println(frame);
+            prevFrame = frame;
+        }
+    } else {
+        static int pacer = 0; 
+        if (pacer++ >= HALL_PACER_MAX) {
+            long reading = analogRead(HALL);
+            // Serial.println(reading);
 
-    default:
-        break;
+            sum = sum - readings[idx];
+            readings[idx] = reading;
+            sum = sum + reading;
+            idx = (idx + 1) % WINDOW_SIZE;
+            averaged = sum / WINDOW_SIZE;
+
+            // Serial.println(reading);
+            Serial.println(averaged);
+            pacer = 0;
+
+            if (averaged >= HALL_THRESHOLD) { // if hall reset switch is deemed close enough
+                // reset animation
+                frame = 1;
+                frameStartTime = micros();
+                prevFrame = 0;
+
+                // reset filter to prevent instantly triggering the next time the animation finishes
+                averaged = 0;
+                sum = 0;
+                for (int i = 0; i<WINDOW_SIZE; i++)
+                {
+                    readings[i] = 0;
+                }
+                
+            }
+        }
     }
-    
-    if (masterFrame > prevFrame) {
-        Serial.print("current time: ");
-        Serial.println(tCurrent);
-        Serial.print("absolute frame: ");
-        Serial.println(masterFrame);
-        Serial.print("animation frame: ");
-        Serial.println(frame);
-        Serial.print("state: ");
-        Serial.println(state);
-        prevFrame = masterFrame;
-        Serial.print("Hall sensor: ");
-        Serial.println(analogRead(HALL));
-    }
 
-//   pixels.clear(); // Set all pixel colors to 'off'
-//   static float brightness = 0;
-//   static int brightnessChange = 1;
-
-//   if (brightness == 255) {
-//     brightnessChange = -1;
-//   } else if (brightness == 0) {
-//     brightnessChange = 1;
-//   }
-
-//   brightness += brightnessChange;
-
-//   float scaler = brightness/255;
-//   colorWipe(pixels.Color(125*scaler,   0,   118*scaler), DELAY); // Red
-//   delay(10);
+    /* ANIMATION */
+    // if (frame > prevFrame) { // only update animation on new frames
+    //     Serial.println("new frame, updating animation");
+        runLever(frame);
+    // runNoseLighting(frame);
+    // runLeverLighting(frame);
+    // }
 }
 
-// void colorWipe(uint32_t color, int wait) {
-//   for(int i=0; i<pixels.numPixels(); i++) { // For each pixel in strip...
-//     pixels.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-//     delay(wait);                           //  Pause for a moment
-//   }
-//     pixels.show();                          //  Update strip to match
-// }
+void runLever(long frame) {
+    if (frame <= STAGE_1_END) {
+        Serial.println("Stage 1");
+        // slow linear to 10 deg
+        // servo.write(SERVO_PIN, 10, 10, 0.0);
+        float target = servoLinear(frame, 0, STAGE_1_END, 0, 20);
+        servo.write(SERVO_PIN, target);
+        Serial.println(target);
+
+    } else if (frame > STAGE_2_START && frame <= STAGE_2_END) {
+        // snap open then ease out to fully open
+        if (frame == STAGE_2_START + 1) {
+            // snap to 40 deg
+            Serial.println("Stage 2 frame 1");
+            servo.write(SERVO_PIN, 40);
+            servoPrevious = 40;
+        } else if (frame >= STAGE_2_START + (FRAMES_PER_STAGE/2)) {
+            // ease to fully open
+            Serial.println("Stage 2");
+            servoTarget = 180;
+            servoSmoothed = servoTarget * SERVO_SMOOTHING + servoPrevious * (1-SERVO_SMOOTHING);
+            servoPrevious = servoSmoothed;
+            // servo.write(SERVO_PIN, 180, 40.0, 0.0);
+            servo.write(SERVO_PIN, servoSmoothed);
+        }
+
+    } else if (frame > STAGE_7_START && frame <= STAGE_7_END) {
+        // ease back to 40 degrees
+        Serial.println("Stage 7");
+        // servo.write(SERVO_PIN, 40, 40.0, 0.66);
+        servo.write(SERVO_PIN, 40);
+
+    } else if (frame > STAGE_8_START && frame <= STAGE_8_END) {
+        if (frame == STAGE_8_START + 1) {
+            Serial.println("Stage 8 frame 1");
+            // snap back to 10 deg
+            servo.write(SERVO_PIN, 10);
+
+        } else if (frame >= STAGE_8_START + (FRAMES_PER_STAGE/2)) {
+            // linear to 0 deg
+            Serial.println("Stage 8");
+            // servo.write(SERVO_PIN, 0, 10, 0.0);
+            servo.write(SERVO_PIN, 0);
+        }
+
+    } else if (frame >= STAGE_9_START) {
+        servo.write(SERVO_PIN, 0);
+        servoSmoothed = 0;
+        servoPrevious = 0;
+    }
+}
+
+float servoLinear(long frame, long startFrame, long endFrame, long startPos, long endPos) {
+    float dPos = (endPos - startPos)/(float(endFrame) - float(startFrame));
+    return (float(frame - startFrame) * dPos);
+}
